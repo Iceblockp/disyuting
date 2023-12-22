288706bec76284f0a0065adaa43d467ff4a2e823
HtutMyatLinn <htutmyatlinnhml18@gmail.com>
2023-12-22 22:02:57 +0630

    disyuting started

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import App from "./App";
 import "./index.css";
 import ReactDOM from "react-dom/client";
+import "../node_modules/flowbite/dist/flowbite";
+// import CountUp from 'react-countup';
 
 const rootEl = document.querySelector("#root");
 const root = ReactDOM.createRoot(rootEl);
