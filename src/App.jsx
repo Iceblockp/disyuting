84d6a69b6996740e068c09fe28d4eb059dc65972
Iceblockp <phonyo126@gmail.com>
2023-12-28 18:37:38 +0630

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 
 const App = () => {
-  return <div className=" ">App</div>;
+  return <div className=" min-h-screen bg-cover bg-center bg-[url('https://templates.casloop.net/disyuting/wp-content/uploads/sites/19/2023/11/Plain-GradientDisyuting.jpg')] ">
+  App
+</div>;
+
 };
 
 export default App;
