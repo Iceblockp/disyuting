2072a66f4fa6cdf0644b9b59b796bf58979816c5
YanNaingYan <yannnai2711@gmail.com>
2023-12-28 18:55:16 +0630

    awardSection added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Awards from "./Awards";
 
 const App = () => {
-  return <div className=" ">App</div>;
+  return (
+    <div className=" min-h-screen  bg-cover bg-center  bg-[url(https://templates.casloop.net/disyuting/wp-content/uploads/sites/19/2023/11/Plain-GradientDisyuting.jpg?fbclid=IwAR1l3hPtEQzywMASaZUJ143jLTh01TkIcSjerpbaaO076i9XAzjXVSHcAyg)]">
+      <Awards />
+    </div>
+  );
 };
 
 export default App;
