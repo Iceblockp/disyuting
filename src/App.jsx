cd6dd35c83c56c9f1de09cbb2c2a50497722b18a
HtutMyatLinn <htutmyatlinnhml18@gmail.com>
2023-12-29 15:28:26 +0630

    second section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import SecSection from "./components/SecSection";
+import SecSection2 from "./components/SecSection2";
 
 const App = () => {
-  return <div className=" ">App</div>;
+  return (
+    <div className=" min-h-screen bg-cover bg-center bg-[url('https://templates.casloop.net/disyuting/wp-content/uploads/sites/19/2023/11/Plain-GradientDisyuting.jpg?fbclid=IwAR0lUiyw4t5Tkdh6zRRhTMWtsAqrUG5A6D6HeH_lyu9NPiW5DKAh6QdMYeg')]">
+      <SecSection />
+      <SecSection2 />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/SecSection.jsx
@@ -0,0 +1,202 @@
+import React from "react";
+import "../../node_modules/aos/dist/aos.css";
+import Aos from "aos";
+Aos.init();
+
+const SecSection = () => {
+  return (
+    <div className="w-full flex mb-[200px]">
+      <div className=" flex mx-0 lg:mx-[5px] w-full">
+        <div className=" w-full lg:w-[91%] mx-auto">
+          <div className=" w-full px-[30px] lg:px-0 py-[10px] lg:py-[5px]">
+            <div className="  w-full grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-4 gap-3 justify-center flex-wrap md:gap-1">
+              {/* first div */}
+              <div
+                data-aos="fade-up"
+                data-aos-duration="1000"
+                className=" hover:shadow-neon-blue shadow-2xl w-full flex items-center justify-center"
+              >
+                <div className=" w-full md:flex bg-white bg-opacity-20 p-1 rounded-sm backdrop-blur-sm">
+                  <div className=" w-full bg-cover md:w-5/12 h-[163px] bg-center md:bg-left-top rounded-lg bg-[url('https://templates.casloop.net/disyuting/wp-content/uploads/sites/19/2023/11/stylish-futuristic-blonde-in-cyberpunk-style-on-a-2022-08-31-22-41-37-utc.jpg')]"></div>
+
+                  <div className="px-4 md:w-7/12">
+                    <div className=" flex justify-between items-center mb-20">
+                      <div className=" text-slate-300">Mv Dance</div>
+                      <div className=" text-slate-300">05 Min</div>
+                    </div>
+
+                    <div className="flex justify-between items-center  text-start">
+                      <div className="">
+                        <div className=" w-[140px]">
+                          <p className=" text-slate-200 font-bold text-xl">
+                            MV Clip DJRedBlack
+                          </p>
+                        </div>
+                      </div>
+                      <div className="">
+                        <a href="#" className="text-white ">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1}
+                            stroke="currentColor"
+                            className="w-[40px] h-[40px] "
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+                            />
+                          </svg>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              {/* sec div */}
+              <div
+                data-aos="fade-up"
+                data-aos-duration="1600"
+                className="hover:shadow-neon-blue  w-full flex items-center justify-center"
+              >
+                <div className=" w-full md:flex bg-white bg-opacity-20 p-1 rounded-sm backdrop-blur-sm">
+                  <div className=" w-full bg-cover md:w-5/12 h-[163px] bg-center md:bg-left-top rounded-lg bg-[url('https://templates.casloop.net/disyuting/wp-content/uploads/sites/19/2023/11/unforgettable-beauty-2022-12-16-14-26-13-utc.jpg')]"></div>
+
+                  <div className="px-4 md:w-7/12">
+                    <div className=" flex justify-between items-center mb-20">
+                      <div className=" text-slate-300">Vlog</div>
+                      <div className=" text-slate-300">20 Min</div>
+                    </div>
+
+                    <div className="flex justify-between items-center  text-start">
+                      <div className="">
+                        <div className=" w-[140px]">
+                          <p className=" text-slate-200  font-bold text-xl">
+                            Beauty Girls With Makeup
+                          </p>
+                        </div>
+                      </div>
+                      <div className="">
+                        <a href="#" className="text-white ">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1}
+                            stroke="currentColor"
+                            className="w-[40px] h-[40px] "
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+                            />
+                          </svg>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* third div */}
+              <div
+                data-aos="fade-up"
+                data-aos-duration="2300"
+                className="hover:shadow-neon-blue  w-full flex items-center justify-center"
+              >
+                <div className=" w-full md:flex bg-white bg-opacity-20 p-1 rounded-sm backdrop-blur-sm">
+                  <div className=" w-full bg-cover md:w-5/12 h-[163px] bg-center md:bg-left-top rounded-lg bg-[url('https://templates.casloop.net/disyuting/wp-content/uploads/sites/19/2023/11/guangzhou-city-2022-12-15-19-29-40-utc.jpg')]"></div>
+
+                  <div className="px-4 md:w-7/12">
+                    <div className=" flex justify-between items-center mb-20">
+                      <div className=" text-slate-300">Mv Dance</div>
+                      <div className=" text-slate-300">05 Min</div>
+                    </div>
+
+                    <div className="flex justify-between items-center  text-start">
+                      <div className="">
+                        <div className="w-[140px]">
+                          <p className=" text-slate-200  font-bold text-xl">
+                            Buildings City Documenter
+                          </p>
+                        </div>
+                      </div>
+                      <div className="">
+                        <a href="#" className="text-white ">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1}
+                            stroke="currentColor"
+                            className="w-[40px] h-[40px] "
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+                            />
+                          </svg>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* fourth div */}
+              <div
+                data-aos="fade-up"
+                data-aos-duration="3000"
+                className="hover:shadow-neon-blue  w-full flex items-center justify-center"
+              >
+                <div className=" w-full md:flex bg-white bg-opacity-20 p-1 rounded-sm backdrop-blur-sm">
+                  <div className=" w-full bg-cover md:w-5/12 h-[163px] bg-center md:bg-left-top rounded-lg bg-[url('https://templates.casloop.net/disyuting/wp-content/uploads/sites/19/2023/11/the-concept-of-shooting-with-gadgets-action-a-lar-2022-12-21-14-26-17-utc.jpg')]"></div>
+
+                  <div className="px-4 md:w-7/12">
+                    <div className=" flex justify-between items-center mb-20">
+                      <div className=" text-slate-300">Mv Dance</div>
+                      <div className=" text-slate-300">05 Min</div>
+                    </div>
+
+                    <div className="flex justify-between items-center  text-start">
+                      <div className="">
+                        <div className="w-[140px]">
+                          <p className=" text-slate-200  font-bold text-xl">
+                            Stabilizer Launching
+                          </p>
+                        </div>
+                      </div>
+                      <div className="">
+                        <a href="#" className="text-white">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1}
+                            stroke="currentColor"
+                            className="w-[40px] h-[40px]"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+                            />
+                          </svg>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SecSection;
