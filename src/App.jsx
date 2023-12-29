5d32aa9dfd0faafa181a84888186b9ccb549b37d
YanNaingYan <yannnai2711@gmail.com>
2023-12-29 19:27:01 +0630

    priceSection added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Awards from "./Awards";
+import Pricelist from "./Pricelist";
 
 const App = () => {
   return (
     <div className=" min-h-screen  bg-cover bg-center  bg-[url(https://templates.casloop.net/disyuting/wp-content/uploads/sites/19/2023/11/Plain-GradientDisyuting.jpg?fbclid=IwAR1l3hPtEQzywMASaZUJ143jLTh01TkIcSjerpbaaO076i9XAzjXVSHcAyg)]">
       <Awards />
+      <Pricelist />
     </div>
   );
 };
