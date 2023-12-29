ca80cdd7fae914aaf177264ec1ae84ae72494549
Iceblockp <phonyo126@gmail.com>
2023-12-29 22:11:39 +0630

    production done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Production from "./components/Production";
 
 const App = () => {
-  return <div className=" min-h-screen bg-cover bg-center bg-[url('https://templates.casloop.net/disyuting/wp-content/uploads/sites/19/2023/11/Plain-GradientDisyuting.jpg')] ">
-  App
-</div>;
-
+  return (
+    <div className=" min-h-screen bg-cover bg-center bg-[url('https://templates.casloop.net/disyuting/wp-content/uploads/sites/19/2023/11/Plain-GradientDisyuting.jpg')] ">
+      <Production />
+    </div>
+  );
 };
 
 export default App;
