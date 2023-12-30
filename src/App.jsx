26c8d7716376549d1aaca4273833edca455d0999
AkoSuu <yehtet232002@gmail.com>
2023-12-30 20:15:03 +0630

    photoInput

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import PhotoInput from "./components/PhotoInput";
 
 const App = () => {
-  return <div className=" ">App</div>;
+  return (
+    <div
+      className=" min-h-screen bg-center bg-cover bg-[url('https://templates.casloop.net/disyuting/wp-content/uploads/sites/19/2023/11/Plain-GradientDisyuting.jpg?fbclid=IwAR2cTMi_Dos6ZGUJeL11iXYQXmL9TlyubyNqdj1HarQSlHuxDxqmc8Ys5ew')]"
+    >
+      <PhotoInput/>
+    </div>
+  );
 };
 
 export default App;
