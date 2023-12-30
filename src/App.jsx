8b246c8feba5a04a9f5f60913c3be36a9b40fe01
Iceblockp <phonyo126@gmail.com>
2023-12-30 16:28:04 +0630

    topvisual done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import TopVisual from "./components/TopVisual";
 
 const App = () => {
   return <div className=" min-h-screen bg-cover bg-center bg-[url('https://templates.casloop.net/disyuting/wp-content/uploads/sites/19/2023/11/Plain-GradientDisyuting.jpg')] ">
-  App
+  <TopVisual/>
 </div>;
 
 };
