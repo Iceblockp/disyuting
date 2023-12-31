bccd4d51bf61fa8e245f2ba36fc73480a9b84795
Iceblockp <phonyo126@gmail.com>
2023-12-31 22:33:56 +0630

    Top

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import AboutTop from "./components/AboutTop";
 
 const App = () => {
   return <div className=" min-h-screen bg-cover bg-center bg-[url('https://templates.casloop.net/disyuting/wp-content/uploads/sites/19/2023/11/Plain-GradientDisyuting.jpg')] ">
-  App
+  <AboutTop/>
 </div>;
 
 };
