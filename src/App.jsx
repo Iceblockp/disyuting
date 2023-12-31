96c24afb20fec212acd6030b294e8b56565718c8
Iceblockp <phonyo126@gmail.com>
2023-12-31 17:47:11 +0630

    test done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import Testimonial from "./components/Testimonial";
 
 const App = () => {
+
+ 
+
   return <div className=" min-h-screen bg-cover bg-center bg-[url('https://templates.casloop.net/disyuting/wp-content/uploads/sites/19/2023/11/Plain-GradientDisyuting.jpg')] ">
-  App
+  
+  
+      <Testimonial/>
 </div>;
 
 };
