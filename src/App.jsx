3569ec69fa9d179444ae4c6ed3e798f307708225
YanNaingYan <yannnai2711@gmail.com>
2024-01-01 18:06:25 +0630

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -11,29 +11,26 @@ import InputFormSection from "./components/InputFormSection";
 import Team from "./components/Team";
 import TopVisual from "./components/TopVisual";
 import Testimonial from "./components/Testimonial";
-
-
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div className=" min-h-screen bg-cover bg-center bg-[url('https://templates.casloop.net/disyuting/wp-content/uploads/sites/19/2023/11/Plain-GradientDisyuting.jpg?fbclid=IwAR0lUiyw4t5Tkdh6zRRhTMWtsAqrUG5A6D6HeH_lyu9NPiW5DKAh6QdMYeg')]">
-     <NavBar/>
-     <Header/>
-     <SecSection />
-     <SecSection2 />
-     <Production />
-     <Awards />
-     <PhotoInput/>
-     <Pricelist />
-     <InputFormSection />
-     <Team />
-     <TopVisual/>
-     <Testimonial/>
+      <NavBar />
+      <Header />
+      <SecSection />
+      <SecSection2 />
+      <Production />
+      <Awards />
+      <PhotoInput />
+      <Pricelist />
+      <InputFormSection />
+      <Team />
+      <TopVisual />
+      <Testimonial />
+      <Footer />
     </div>
   );
 };
-
-
-
 
 export default App;
