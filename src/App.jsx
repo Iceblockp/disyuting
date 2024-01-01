74fdd896a0ea1d6c7c50c1e2752941bed43e6dda
YanNaingYan <yannnai2711@gmail.com>
2024-01-01 20:20:48 +0630

    award n price animation added

--- src/App.jsx
+++ src/App.jsx
@@ -13,45 +13,88 @@ import Awards from "./components/Awards";
 import PhotoInput from "./components/PhotoInput";
 import Pricelist from "./components/Pricelist";
 import InputFormSection from "./components/InputFormSection";
-import ShortMovie from "./components/ShortMovie";
 import Team from "./components/Team";
 import TopVisual from "./components/TopVisual";
 import Testimonial from "./components/Testimonial";
-import { Routes,Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Map from "./components/Map";
 
 const App = () => {
   return (
-
-        <div className=" min-h-screen bg-cover bg-center bg-[url('https://templates.casloop.net/disyuting/wp-content/uploads/sites/19/2023/11/Plain-GradientDisyuting.jpg?fbclid=IwAR0lUiyw4t5Tkdh6zRRhTMWtsAqrUG5A6D6HeH_lyu9NPiW5DKAh6QdMYeg')]">
-        <NavBar/>
-    <Routes>
-      <Route path="/" element={<><Header /> 
-      <SecSection />
-      <SecSection2 />
-      <Production />
-      <Awards />
-      <PhotoInput />
-      <Pricelist />
-      <InputFormSection />
-      <Team />
-      <TopVisual />
-      <Testimonial />
-      <Map />
-      <Footer /></>}></Route>
-      <Route path="about" element={<><AboutTop /><AboutSecond />< AboutJourney /><Awards /><Team /><AboutAccordian /><InputFormSection /><Footer /></>} ></Route>
-      <Route path="ourProject" element={<><AboutTop /><Header /><TopVisual /><Testimonial /><Footer /></>} ></Route>
-      <Route path="pages" element={<><AboutTop /><PhotoInput />   <ShortMovie />  <Pricelist /> <AboutAccordian /> <Footer /></>} ></Route>
-      <Route path="contactUs" element={<><AboutTop />   <AboutSecond /><Testimonial /><Map /><Footer /></>} ></Route>
+    <div className=" min-h-screen bg-cover bg-center bg-[url('https://templates.casloop.net/disyuting/wp-content/uploads/sites/19/2023/11/Plain-GradientDisyuting.jpg?fbclid=IwAR0lUiyw4t5Tkdh6zRRhTMWtsAqrUG5A6D6HeH_lyu9NPiW5DKAh6QdMYeg')]">
+      <NavBar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <SecSection />
+              <SecSection2 />
+              <Production />
+              <Awards />
+              <PhotoInput />
+              <Pricelist />
+              <InputFormSection />
+              <Team />
+              <TopVisual />
+              <Testimonial />
+              <Map />
+              <Footer />
+            </>
+          }
+        ></Route>
+        <Route
+          path="about"
+          element={
+            <>
+              <AboutTop />
+              <AboutSecond />
+              <AboutJourney />
+              <Awards />
+              <Team />
+              <AboutAccordian />
+              <InputFormSection />
+              <Footer />
+            </>
+          }
+        ></Route>
+        <Route
+          path="ourProject"
+          element={
+            <>
+              <AboutTop />
+              <Header />
+              <TopVisual />
+              <Testimonial />
+              <Footer />
+            </>
+          }
+        ></Route>
+        <Route
+          path="pages"
+          element={
+            <>
+              <AboutTop />
+              <PhotoInput /> <Pricelist /> <AboutAccordian /> <Footer />
+            </>
+          }
+        ></Route>
+        <Route
+          path="contactUs"
+          element={
+            <>
+              <AboutTop /> <AboutSecond />
+              <Testimonial />
+              <Map />
+              <Footer />
+            </>
+          }
+        ></Route>
       </Routes>
-      
-
     </div>
-    
   );
 };
-
-   
 
 export default App;
